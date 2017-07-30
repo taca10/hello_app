a3a2d8a1759e6ac1bfd7819a3ca80a9c95a26f8b
taca10 <taka.ur.0505@gmail.com>
2017-07-30 10:59:22 +0900

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages#home'
+  get 'foo/bar'
+  get 'foo/baz'
 
+  get 'static_pages/home'
   get 'static_pages/help'
+  get 'static_pages/about'
+  get 'static_pages/contact'
 
   resources :microposts
   resources :users
-  root 'users#index'
 end
